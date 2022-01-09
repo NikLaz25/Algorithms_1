01e258797bfa22cf06eb6e6a2ad604b958eca51b
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-01-10 01:25:45 +0300

    Тесты для связанных списков

--- LinkedList_test_1.py
+++ LinkedList_test_1.py
@@ -1,7 +1,7 @@
 '''проверочные тесты'''
 
 import unittest
-from LinkedList_3 import Node, LinkedList
+from LinkedList_4 import Node, LinkedList
 
 class TestCase_1(unittest.TestCase):
     '''Тестирование'''
@@ -198,6 +198,25 @@ class TestCase_1(unittest.TestCase):
         result = s_list.LinkedList_in_List()
         answer = [5]
         self.assertEqual(result, answer)
+
+    def test_insert_None(self):
+        '''тестируем insert_None вставка значения в пустой список'''
+        s_list = LinkedList()
+
+        s_list.insert(None, 15)
+        result_1 = s_list.LinkedList_in_List()
+        answer_1 = [15]
+        result_2 = s_list.head.value
+        answer_2 = 15
+        result_3 = s_list.tail.value
+        answer_3 = 15
+        result_4 = s_list.tail.next
+        answer_4 = None
+        self.assertEqual(result_1, answer_1)
+        self.assertEqual(result_2, answer_2)
+        self.assertEqual(result_3, answer_3)
+        self.assertEqual(result_4, answer_4)
+
 if __name__ == '__main__':
     unittest.main()
 
