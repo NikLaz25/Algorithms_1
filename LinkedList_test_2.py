60907a6510220c9a1cfa48f1580ef9308458aec0
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-01-10 01:19:00 +0300

    For LinkedList_4

--- LinkedList_test_2.py
+++ LinkedList_test_2.py
@@ -9,10 +9,19 @@ class TestCase_1(unittest.TestCase):
         '''тестируем insert_None вставка значения в пустой список'''
         s_list = LinkedList()
 
-        s_list.insert(None, 5)
-        result = s_list.LinkedList_in_List()
-        answer = [5]
-        self.assertEqual(result, answer)
+        s_list.insert(None, 15)
+        result_1 = s_list.LinkedList_in_List()
+        answer_1 = [15]
+        result_2 = s_list.head.value
+        answer_2 = 15
+        result_3 = s_list.tail.value
+        answer_3 = 15
+        result_4 = s_list.tail.next
+        answer_4 = None
+        self.assertEqual(result_1, answer_1)
+        self.assertEqual(result_2, answer_2)
+        self.assertEqual(result_3, answer_3)
+        self.assertEqual(result_4, answer_4)
 
 
 
