b0fe650a72fdefdd546fb8b77fd2cf8af8b17a04
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-02-06 13:11:59 +0300

    Add files via upload

--- queue_test.py
+++ queue_test.py
@@ -3,6 +3,7 @@ from queue_1 import Queue
 
 class MyTestCase(unittest.TestCase):
     def test_rotation(self):
+        '''тестируем rotation'''
         qu = Queue()
         qu.enqueue(1)
         qu.enqueue(2)
@@ -14,7 +15,25 @@ class MyTestCase(unittest.TestCase):
         answer = [3, 4, 1, 2]
         self.assertEqual(result, answer)
 
+    def test_rotation_one(self):
+        '''тестируем rotation когда элемент один в очереди'''
+        qu = Queue()
+        qu.enqueue(1)
+
+        result = qu.LinkedList_in_List()
+        answer = [1]
+        self.assertEqual(result, answer)
+
+    def test_rotation_none(self):
+        '''тестируем rotation очередь пуста'''
+        qu = Queue()
+
+        result = qu.LinkedList_in_List()
+        answer = []
+        self.assertEqual(result, answer)
+
     def test_dequeue(self):
+        '''тестируем dequeue'''
         qu = Queue()
         qu.enqueue(1)
         qu.enqueue(2)
@@ -25,7 +44,25 @@ class MyTestCase(unittest.TestCase):
         answer = 1
         self.assertEqual(result, answer)
 
+    def test_dequeue_one(self):
+        '''тестируем dequeue когда элемент один в очереди'''
+        qu = Queue()
+        qu.enqueue(1)
+
+        result = qu.dequeue()
+        answer = 1
+        self.assertEqual(result, answer)
+
+    def test_dequeue_one(self):
+        '''тестируем dequeue когда очередь пуста'''
+        qu = Queue()
+
+        result = qu.dequeue()
+        answer = None
+        self.assertEqual(result, answer)
+
     def test_size(self):
+        '''тестируем size'''
         qu = Queue()
         qu.enqueue(1)
         qu.enqueue(2)
@@ -35,5 +72,23 @@ class MyTestCase(unittest.TestCase):
         result = qu.size()
         answer = 4
         self.assertEqual(result, answer)
+
+    def test_size(self):
+        '''тестируем size когда элемент один в очереди'''
+        qu = Queue()
+        qu.enqueue(1)
+
+        result = qu.size()
+        answer = 1
+        self.assertEqual(result, answer)
+
+    def test_size(self):
+        '''тестируем size когда очередь пуста'''
+        qu = Queue()
+
+        result = qu.size()
+        answer = 0
+        self.assertEqual(result, answer)
+
 if __name__ == '__main__':
     unittest.main()
