6bf9dfa97407585c35c202330f7c7b5a8dd7d779
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-03-12 13:00:01 +0300

    Add files via upload

--- BloomFilter_1.py
+++ BloomFilter_1.py
@@ -1,5 +1,5 @@
 '''
-фильтр Блюма, вариант 1
+фильтр Блюма, вариант 2
 '''
 
 
@@ -9,42 +9,45 @@ class BloomFilter:
     def __init__(self, f_len):
         '''создаём битовый массив длиной f_len '''
         self.filter_len = f_len
-        self.bit_massiv = '{:01b}'.format(0) * self.filter_len
+        self.bit_massiv = 0b10 ** self.filter_len
 
     def hash1(self, str1):
         '''hash1'''
-        result = 0
+        index = 0
         rand = 17
         for c in str1:
             code = ord(c)
-            result = abs(result * rand + code)  # 223
-            x = result // 32
-            result = result - x * 32
-        return result
+            index = abs(index * rand + code)  # 223
+            x = index // 32
+            index = index - x * 32
+        mask1 = 0b10 ** (32 - 1 - index)
+        return mask1
 
     # выдаёт только одну, единственную позицию, индекс, в массиве
     # выдаёт битовую маску, в которой в позиции этого индекса выставлена единичка
 
     def hash2(self, str1):
         '''hash2'''
-        result = 0
+        index = 0
         rand = 223
         for c in str1:
             code = ord(c)
-            result = abs(result * rand + code)  # 223
-            x = result // 32
-            result = result - x * 32
-        return result
+            index = abs(index * rand + code)  # 223
+            x = index // 32
+            index = index - x * 32
+        mask2 = 0b10 ** (32 - 1 - index)
+        return mask2
 
     def add(self, str1):
         '''добавляем строку str1 в фильтр'''
-        self.bit_massiv = self.bit_massiv[:self.hash1(str1)] + '1' + self.bit_massiv[self.hash1(str1) + 1:]
-        self.bit_massiv = self.bit_massiv[:self.hash2(str1)] + '1' + self.bit_massiv[self.hash2(str1) + 1:]
+        self.bit_massiv = self.bit_massiv | self.hash1(str1)
+        self.bit_massiv = self.bit_massiv | self.hash2(str1)
 
     def is_value(self, str1):
         '''проверка, имеется ли строка str1 в фильтре'''
-        hash_1 = int(self.hash1(str1))
-        hash_2 = int(self.hash2(str1))
-        if self.bit_massiv[hash_1] == '1' and self.bit_massiv[hash_2] == '1':
+        index1 = 31 - len(bin(self.hash1(str1))) + 3 + 3
+        index2 = 31 - len(bin(self.hash2(str1))) + 3 + 3
+        if bin(self.bit_massiv)[index1] == '1' and bin(self.bit_massiv)[index2] == '1':
             return True
         return False
+
