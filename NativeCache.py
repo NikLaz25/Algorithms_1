a27143753dde41edd71ed83fbd700d8658474b67
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-03-14 10:02:44 +0300

    Add files via upload

---
+++ NativeCache.py
@@ -0,0 +1,71 @@
+'''
+Кэш
+(за основу взяты методы ассоциативного массива, но с учетом корректировок)
+
+'''
+
+class NativeCache:
+    def __init__(self, sz):
+        '''Контструктор'''
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+        self.hits = [0] * self.size
+
+    def hash_fun(self, key):  # в качестве key поступают строки!
+        '''по входному значению вычисляет индекс слота'''
+        len_key = len(key)
+        len_slots = len(self.slots)
+        if len_key > len_slots:
+            index = len_key - (len_key // len_slots) * len_slots - 1
+        else:
+            index = len_key - 1
+        return index  # всегда возвращает корректный индекс слота
+
+    def is_key(self, key):
+        '''возвращает True если ключ имеется, иначе False'''
+        for index in self.slots:
+            if index == key:
+                return True
+        return False
+
+    def put(self, key, value):
+        '''гарантированно записываем значение value по ключу key'''
+        index = self.seek_slot(key)
+        my_key = self.is_key(key)
+        if my_key is False:
+            self.slots[index] = key
+        self.values[index] = value
+
+    def get(self, key):
+        '''возвращает value для key, или None если ключ не найден'''
+        for index, key_val in enumerate(self.slots):
+            if key_val == key:
+                self.hits[index] += 1
+                return self.values[index]
+        return None
+
+    def seek_slot(self, value):
+        '''функцию поиска слота - по входному значению сперва рассчитывает индекс хэш-функцией,
+        а затем отыскивает подходящий слот для него с учётом коллизий,
+        или возвращает None, если это не удалось'''
+        # находит индекс пустого слота для значения, или None
+        step = 3
+        index = self.hash_fun(value)
+        len_slots = len(self.slots)
+        if self.slots[index] is None or self.slots[index] == value:
+            return index
+        else:
+            check_slot_counter = 0
+            while self.slots[index] is not None:
+                check_slot_counter += 1
+                if self.slots[index] == value:
+                    break
+                elif (index + 1 + step) <= len_slots and check_slot_counter < len_slots:
+                    index += step
+                elif (index + 1 + step) > len_slots and check_slot_counter < len_slots:
+                    index = index + step - len(self.slots)
+                elif check_slot_counter >= len_slots:
+                    min_index_get = self.hits.index(min(self.hits))
+                    return min_index_get
+        return index
