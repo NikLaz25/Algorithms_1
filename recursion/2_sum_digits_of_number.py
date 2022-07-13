375641678d70e53fbfb058c81e93bbc967d63a8e
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-13 17:26:53 +0300

    внесены корректировки.
    избавились от try-except

--- recursion/2_sum_digits_of_number.py
+++ recursion/2_sum_digits_of_number.py
@@ -1,12 +1,11 @@
 # рекурсивный метод вычисление суммы цифр числа
 def sum_digits_of_number(number):
-    if len(str(number)) > 0:
+    if len(str(number)) > 1:
         str_number = str(number)
         digit = int(str_number[0])
-        try:
-            remaining_number = int(str_number[1:])
-            return digit + sum_digits_of_number(remaining_number)
-        except:
-            return digit
+        remaining_number = int(str_number[1:])
+        return digit + sum_digits_of_number(remaining_number)
+    else:
+        return number
 
 sum_digits_of_number(333)
