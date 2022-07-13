3c637a2ffcf8db55a7f96b0fbbb91506a3271ea5
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-13 17:28:29 +0300

    # внесены корректировки избавились от try-except

--- recursion/3_length_of_the_list.py
+++ recursion/3_length_of_the_list.py
@@ -1,10 +1,10 @@
-# расчёт длины списка, для которого разрешена только одна операция удаления первого элемента pop(0)
-
+# расчёт длины списка, для которого разрешена 
+# только одна операция удаления первого элемента pop(0)
 def length_of_the_list(work_list):
-    try:
+    if work_list:
         work_list.pop(0)
         return 1 + length_of_the_list(work_list)
-    except:
+    else:
         return 0
 
 work_list = [1, 2, 4, 3, 2, 1, 2]
