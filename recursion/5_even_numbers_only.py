2b22368805ed0da75c59a6384b34220bfbcf3030
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-13 17:29:16 +0300

    # внесены корректировки избавились от try-except

--- recursion/5_even_numbers_only.py
+++ recursion/5_even_numbers_only.py
@@ -1,11 +1,11 @@
 # печать только чётных значений из списка
-
 def even_numbers_only(work_list):
-    if work_list[0] % 2 == 0:
+    if work_list and work_list[0] % 2 == 0 :
         print(work_list[0])
-    try:
         even_numbers_only(work_list[1:])
-    except:
+    elif work_list and work_list[0] % 2 != 0 :
+        even_numbers_only(work_list[1:])
+    else:
         pass
 
 work_list = [1, 2, 4, 3, 2, 1, 2, 3]
