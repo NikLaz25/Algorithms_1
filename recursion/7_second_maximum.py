72801dffd5caddccc279d3c98ee96e2610109d32
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-13 17:33:11 +0300

    # внесены корректировки избавились от sorted()

--- recursion/7_second_maximum.py
+++ recursion/7_second_maximum.py
@@ -1,12 +1,20 @@
-# нахождение второго максимального числа в списке (с учётом, что максимальных может быть несколько, если они равны)
+# нахождение второго максимального числа в списке 
+#(с учётом, что максимальных может быть несколько, если они равны)
+def second_maximum(work_list, first_max, second_max):
+    
+    if len(work_list) == 0:
+        return second_max
+    elif len(work_list) != 0 and work_list[0] < first_max and work_list[0] > second_max:
+        second_max = work_list[0]
+    elif len(work_list) != 0 and work_list[0] < first_max and work_list[0] <= second_max:
+        pass
 
-def second_maximum(work_list, max_in_list):
-    work_list = sorted(work_list)[::-1]
-    if work_list[0] == max_in_list:
-        work_list = work_list[1:]
-        return second_maximum(work_list, max_in_list)
-    else:
-        return work_list[0]
+    second_max = second_maximum(work_list[1:], first_max, second_max)
 
-work_list = [1, 2, 4, 5, 5, 3, 9, 2, 1, 2, 9, 3, 5, 7, 8, 8, 8]
-second_maximum(work_list, max(work_list))
+    return second_max
+
+work_list = [-1, -2, -4, -5, -5, -1]
+print(second_maximum(work_list, max(work_list), min(work_list)))
+
+work_list = [1, 2, 4, 5, 5, 3, 2, 1, 2, 3, 5, 7, 8, 8, 8]
+print(second_maximum(work_list, max(work_list), min(work_list)))
