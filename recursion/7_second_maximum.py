0f6e168cb8aa31adac2d641a974451aae0a982a9
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-14 18:07:40 +0300

    внесены очередные корректировки
    только для неотрицательных значений

--- recursion/7_second_maximum.py
+++ recursion/7_second_maximum.py
@@ -1,20 +1,16 @@
 # нахождение второго максимального числа в списке 
 #(с учётом, что максимальных может быть несколько, если они равны)
-def second_maximum(work_list, first_max, second_max):
-    
+def second_maximum(work_list, first_max=0, second_max=0):
     if len(work_list) == 0:
         return second_max
-    elif len(work_list) != 0 and work_list[0] < first_max and work_list[0] > second_max:
+    elif work_list[0] > first_max:
+        second_max, first_max = first_max, work_list[0]
+    elif second_max < work_list[0] < first_max:
         second_max = work_list[0]
-    elif len(work_list) != 0 and work_list[0] < first_max and work_list[0] <= second_max:
-        pass
 
     second_max = second_maximum(work_list[1:], first_max, second_max)
-
+    
     return second_max
-
-work_list = [-1, -2, -4, -5, -5, -1]
-print(second_maximum(work_list, max(work_list), min(work_list)))
 
 work_list = [1, 2, 4, 5, 5, 3, 2, 1, 2, 3, 5, 7, 8, 8, 8]
 print(second_maximum(work_list, max(work_list), min(work_list)))
