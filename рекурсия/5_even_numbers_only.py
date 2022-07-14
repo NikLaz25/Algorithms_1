14d925906f37ca743a567b0e572498c5573c12e3
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-14 18:00:02 +0300

    # внесены повторные корректировки убрали ненужное условие

--- рекурсия/5_even_numbers_only.py
+++ рекурсия/5_even_numbers_only.py
@@ -1,12 +1,10 @@
 # печать только чётных значений из списка
 def even_numbers_only(work_list):
-    if work_list and work_list[0] % 2 == 0 :
+    if len(work_list) == 0:
+        return
+    elif work_list[0] % 2 == 0 :
         print(work_list[0])
-        even_numbers_only(work_list[1:])
-    elif work_list and work_list[0] % 2 != 0 :
-        even_numbers_only(work_list[1:])
-    else:
-        pass
+    even_numbers_only(work_list[1:])
 
 work_list = [1, 2, 4, 3, 2, 1, 2, 3]
 even_numbers_only(work_list)
