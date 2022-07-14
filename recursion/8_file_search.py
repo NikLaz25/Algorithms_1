358138987ea656cc696aadc93463f311236421d6
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-14 18:09:44 +0300

    внесли корректировки, упрощен алгоритм

--- recursion/8_file_search.py
+++ recursion/8_file_search.py
@@ -1,37 +1,15 @@
 # поиск всех файлов в заданном каталоге, включая файлы, расположенные в подкаталогах произвольной вложенности
+import os
 
 def file_search(catalog_name):
+    # Создаём локальный список  и заносим в него все файлы в текущем каталоге.
+    file_list = [i for i in os.listdir(catalog_name) if os.path.isfile(os.path.join(catalog_name, i))]
+    # Цикл, для каждого из подкаталогов текущего каталога  
+    for i in [(os.path.join(catalog_name, i)) for i in (os.listdir(catalog_name)) if (os.path.isdir(os.path.join(catalog_name, i)))]:
+        file_list_for_folder = file_search(i) # вызываем рекурсивно функцию, которая возвращает файлы в указанном подкаталоге.
+        file_list += file_list_for_folder # добавляем их в список 
 
-    # получим список объектов в заданном каталоге
-    content = os.listdir(catalog_name)
-
-    # переформатируем список объектов в список адресов
-    adres_list = []
-    for i in content:
-        i_adres = os.path.join(catalog_name, i)
-        adres_list.append(i_adres)
-
-    # разобъём объекты на список адресов папок и адресов файлов
-    file_adres_list = []
-    folder_adres_list = []
-    for i in adres_list:
-        if os.path.isfile(i) is True:
-            file_adres_list.append(i)
-        else:
-            folder_adres_list.append(i)
-
-    # ЦИКЛ пополним file_list файлами из текущей директории
-    file_list=[]
-    for i in file_adres_list:
-        file_name = os.path.basename(i)
-        file_list.append(file_name)
-
-    # ЦИКЛом для каждой папки вызовем рекурсивный метод 
-    # и добавим результат в file_list
-    for i in folder_adres_list:
-        file_list_for_folder = file_search(i)
-        file_list += file_list_for_folder
-    
+    # Цикл закончился — делаем return
     return file_list
 
 adres = r'C:\Users\lazarevnv\OneDrive\1_Обучение\Алгоритмы\рекурсия'
