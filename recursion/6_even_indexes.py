fd6a5a79b6134f1009636f3ad3600b7226bc30f1
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-14 18:03:43 +0300

    внесены повторные корректировки, убрал лишнее условие

--- recursion/6_even_indexes.py
+++ recursion/6_even_indexes.py
@@ -1,12 +1,11 @@
 # печать элементов списка с чётными индексами
 def even_indexes(work_list, index=0):
-    if index < len(work_list) and index % 2 == 0:
+    if index == len(work_list):
+        return
+    elif index < len(work_list) and index % 2 == 0:
         print(work_list[index])
-        even_indexes(work_list, index + 1)
-    elif index < len(work_list) and index % 2 != 0:
-        even_indexes(work_list, index + 1)
-    else: 
-        pass
+
+    even_indexes(work_list, index + 1)
 
 work_list = [1, 2, 4, 3, 2, 1, 2, 3]
 even_indexes(work_list)
