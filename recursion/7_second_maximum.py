0ad53472d7114fb8787a0e6e1bc28d489574d712
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-17 10:22:18 +0300

    Изменилась логика алгоритма

--- recursion/7_second_maximum.py
+++ recursion/7_second_maximum.py
@@ -1,16 +1,35 @@
 # нахождение второго максимального числа в списке 
 #(с учётом, что максимальных может быть несколько, если они равны)
-def second_maximum(work_list, first_max=0, second_max=0):
-    if len(work_list) == 0:
-        return second_max
-    elif work_list[0] > first_max:
-        second_max, first_max = first_max, work_list[0]
-    elif second_max < work_list[0] < first_max:
-        second_max = work_list[0]
+def second_maximum(work_list):
+    if len(work_list) >= 3 and (work_list[2] == work_list[0] or work_list[2] == work_list[1]):
+        del work_list[2]
+        return second_maximum(work_list)
 
-    second_max = second_maximum(work_list[1:], first_max, second_max)
+    elif len(work_list) >= 3 and work_list[1] < work_list[0] < work_list[2]:
+        work_list[1] = work_list[0]
+    elif len(work_list) >= 3 and work_list[0] < work_list[2] < work_list[1]:
+        work_list[0] = work_list[2]
+        work_list[2] = work_list[1]
+        work_list[1] = work_list[0]
+    elif len(work_list) >= 3 and work_list[1] < work_list[2] < work_list[0]:
+        work_list[1] = work_list[2]
+        work_list[2] = work_list[0]
+    elif len(work_list) >= 3 and work_list[2] < work_list[0] < work_list[1]:
+        work_list[2] = work_list[1]
+        work_list[1] = work_list[0]
+    elif len(work_list) >= 3 and work_list[2] < work_list[1] < work_list[0]:
+        work_list[2] = work_list[0]    
     
-    return second_max
+    # завершение работы со списком
+    elif len(work_list) == 2 and work_list[0] < work_list[1]:
+        return work_list[0]
+    elif len(work_list) == 2 and work_list[1] < work_list[0]:
+        return work_list[1]
+    elif len(work_list) < 2:
+        print('Длина списка не соответствует требованиям')
+        return
+    
+    return second_maximum(work_list[1:])
 
 work_list = [1, 2, 4, 5, 5, 3, 2, 1, 2, 3, 5, 7, 8, 8, 8]
 print(second_maximum(work_list))
