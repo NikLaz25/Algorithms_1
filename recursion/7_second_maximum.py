28c0367fd80dacf1156cb9bafbecb55e6d344d0f
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-18 19:49:13 +0300

    Использован алгоритм пузырьковой сортировки

--- recursion/7_second_maximum.py
+++ recursion/7_second_maximum.py
@@ -1,35 +1,12 @@
 # нахождение второго максимального числа в списке 
 #(с учётом, что максимальных может быть несколько, если они равны)
 def second_maximum(work_list):
-    if len(work_list) >= 3 and (work_list[2] == work_list[0] or work_list[2] == work_list[1]):
-        del work_list[2]
-        return second_maximum(work_list)
-
-    elif len(work_list) >= 3 and work_list[1] < work_list[0] < work_list[2]:
-        work_list[1] = work_list[0]
-    elif len(work_list) >= 3 and work_list[0] < work_list[2] < work_list[1]:
-        work_list[0] = work_list[2]
-        work_list[2] = work_list[1]
-        work_list[1] = work_list[0]
-    elif len(work_list) >= 3 and work_list[1] < work_list[2] < work_list[0]:
-        work_list[1] = work_list[2]
-        work_list[2] = work_list[0]
-    elif len(work_list) >= 3 and work_list[2] < work_list[0] < work_list[1]:
-        work_list[2] = work_list[1]
-        work_list[1] = work_list[0]
-    elif len(work_list) >= 3 and work_list[2] < work_list[1] < work_list[0]:
-        work_list[2] = work_list[0]    
-    
-    # завершение работы со списком
-    elif len(work_list) == 2 and work_list[0] < work_list[1]:
-        return work_list[0]
-    elif len(work_list) == 2 and work_list[1] < work_list[0]:
-        return work_list[1]
-    elif len(work_list) < 2:
-        print('Длина списка не соответствует требованиям')
-        return
-    
-    return second_maximum(work_list[1:])
+    swapped = False
+    for i in range(len(work_list) - 1):
+        if work_list[i] > work_list[i + 1]:
+            work_list[i], work_list[i + 1] = work_list[i + 1], work_list[i]
+            swapped = True
+    return second_maximum(work_list) if swapped else (list(set(work_list))[-2]) 
 
 work_list = [1, 2, 4, 5, 5, 3, 2, 1, 2, 3, 5, 7, 8, 8, 8]
 print(second_maximum(work_list))
