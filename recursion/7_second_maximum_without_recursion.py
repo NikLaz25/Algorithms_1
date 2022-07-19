13268b53ca1c0968de165282e8c7fac104630985
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-19 14:07:18 +0300

    Update 7_second_maximum_without_recursion.py

--- recursion/7_second_maximum_without_recursion.py
+++ recursion/7_second_maximum_without_recursion.py
@@ -1,10 +1,15 @@
 def second_maximum(work_list):
-    first_max_value = work_list[0]
-    for i in work_list:
-        if i > first_max_value:
-            first_max_value, second_max_value= i, first_max_value
 
-    return second_max_value
+    for x in range(2):
+        max_value = work_list[0]
+        for i in work_list:
+            if i > max_value:
+                max_value = i
 
-work_list = [1, 2, 4, 5, 5, 3, 2, 2, 2, 3, 5, 7, 8, 8, 8]
+        while max_value in work_list:
+            work_list.remove(max_value)
+
+    return max_value
+
+work_list = [8, 2, 4, 5, 5, 3, 2, 2, 2, 3, 5, 7, 8, 8, 8]
 second_maximum(work_list)
