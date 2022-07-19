ecc2b48fea41dc758bb4d9ee68b9de5f4cb51a0a
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-19 12:31:59 +0300

    Update 7_second_maximum_without_recursion.py

--- recursion/7_second_maximum_without_recursion.py
+++ recursion/7_second_maximum_without_recursion.py
@@ -1,13 +1,10 @@
-# пузырьковая сортировка без рекурсии
 def second_maximum(work_list):
-    swapped = True
-    while swapped:
-        swapped = False
-        for i in range(len(work_list) - 1):
-            if work_list[i] > work_list[i + 1]:
-                work_list[i], work_list[i + 1] = work_list[i + 1], work_list[i]
-                swapped = True
-    return (list(set(work_list))[-2])
+    first_max_value = work_list[0]
+    for i in work_list:
+        if i > first_max_value:
+            first_max_value, second_max_value= i, first_max_value
+
+    return second_max_value
 
 work_list = [1, 2, 4, 5, 5, 3, 2, 2, 2, 3, 5, 7, 8, 8, 8]
 second_maximum(work_list)
