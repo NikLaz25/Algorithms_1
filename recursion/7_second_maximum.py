62d8238eed81964a965755073b3cccab91339fa6
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2022-07-19 17:42:13 +0300

    Update 7_second_maximum.py

--- recursion/7_second_maximum.py
+++ recursion/7_second_maximum.py
@@ -1,21 +1,18 @@
 # нахождение второго максимального числа в списке 
 #(с учётом, что максимальных может быть несколько, если они равны)
 
-def second_maximum_recursion(work_list, function_call_count):
-    '''рекурсивный метод нахождения максимума, ограничен двумя итерациями'''
-    max_value = work_list[0]
-    for i in work_list:
-        if i > max_value:
-            max_value = i
-
-    while max_value in work_list:
-        work_list.remove(max_value)
-
-    return max_value if function_call_count >= 2 else  second_maximum_recursion(work_list, function_call_count + 1)
+def second_maximum_recursion(work_list, first_max_value, second_max_value):
+    '''рекурсивный метод нахождения второго максимума'''
+    if work_list[0] > first_max_value:
+        first_max_value, second_max_value = work_list[0], first_max_value
+    elif second_max_value < work_list[0] < first_max_value or (first_max_value == second_max_value and work_list[0] < second_max_value):
+        second_max_value = work_list[0]
+    return second_max_value if len(work_list) == 1 else second_maximum_recursion(work_list[1:], first_max_value, second_max_value)
 
 def second_maximum(work_list):
     '''нахождение второго максимального числа в списке'''
-    second_max_value = second_maximum_recursion(work_list, 1)
+    first_max_value, second_max_value = work_list[0], work_list[0]
+    second_max_value = second_maximum_recursion(work_list, first_max_value, second_max_value)
     return second_max_value
 
 work_list = [8, 2, 4, 5, 5, 3, 2, 1, 2, 3, 5, 7, 8, 8, 8]
